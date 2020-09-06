c7d34bce14e655c03295cf312f2322771aa55989
DavidJoacaRo <davidjoacaro99.dd95@gmail.com>
2020-09-06 17:46:00 +0300

    Implemented API

--- modules/imageCommands.js
+++ modules/imageCommands.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { MessageAttachment } = require('discord.js');
 const randomPuppy = require('random-puppy'); //Because puppies are nice.
 const { client, PREFIX } = require('../index'); // Import client from index.js
 const fetch = require('node-fetch');
@@ -6,13 +7,6 @@ const fetch = require('node-fetch');
 client.on('message', async (message) => 
 {
     if (message.author.bot) return;
-    
-    if (message.content.toLowerCase().startsWith(`${PREFIX}sbubby`)) {
-        randomPuppy('sbubby').then(url => {
-            message.channel.send(url);
-        });
-        return;
-    }
 
     if (message.content.toLowerCase().startsWith(`${PREFIX}dummyimage`)) {
         const args = message.content.slice(PREFIX.length).trim().split(' ');
@@ -22,94 +16,89 @@ client.on('message', async (message) =>
         message.channel.send('https://dummyimage.com/' + width + '/' + '000/fff.png&text=' + forrealtext + '+');
         return;
     }
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}animeme`)) 
-	{
-        randomPuppy('goodanimemes').then(url => {
-			message.channel.send(url);
-        });  
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}borgar`)) {
+        const borgarembed = new Discord.MessageEmbed()
+            .setTitle('Borgar')
+            .setURL('https://youtu.be/LqL1Pm5-uek')
+            .setImage('https://cdn.discordapp.com/attachments/736196476837036102/749324531943997502/borgar.PNG')
+            .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+        message.channel.send(borgarembed);
         return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}pic`)) 
-	{
-		randomPuppy('pics').then(url => {
-			message.channel.send(url);
-        });  
-        return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}comedyheaven`)) {
-		randomPuppy('comedyheaven').then(url => {
-			message.channel.send(url);
-        });  
-        return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}dankmeme`)) 
-	{
-		randomPuppy('dankmemes').then(url => {
-			message.channel.send(url);
-        });  
-        return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}4chan`)) 
-	{
-		randomPuppy('greentext').then(url => {
-			message.channel.send(url);
-        });  
-        return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}requestavatar`)) 
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}requestavatar`)) 
     {
-        message.channel.send("Here is your requested avatar!");
-        message.channel.send(message.author.displayAvatarURL());	
+        const avatarembed = new Discord.MessageEmbed()
+            .setTitle('Here is your requested avatar!')
+            .setURL('https://youtu.be/TYqEcxfYMt0')
+            .setImage(message.author.displayAvatarURL({ format: 'png', size: 512 }))
+            .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+        message.channel.send(avatarembed);
         return;
 	}
 	
 	if (message.content.toLowerCase().startsWith(`${PREFIX}evaxephon`)) 
 	{
-        message.channel.send("https://yandere-simulator.com/tampon.png");	
+        const evaembed = new Discord.MessageEmbed()
+            .setTitle('I, EvaSex')
+            .setURL('https://youtu.be/dxnevAItvFM')
+            .setImage('https://yandere-simulator.com/tampon.png')
+            .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+        message.channel.send(evaembed);
         return;
-	}
-	
-	if (message.content.toLowerCase().startsWith(`${PREFIX}puppy`)) 
-	{
-		randomPuppy().then(url => {
-			message.channel.send(url);
-        });
+    }
+    
+    if (message.content.toLowerCase().startsWith(`${PREFIX}trigger`)) {
+        const tigerembed = new Discord.MessageEmbed()
+            .setTitle('Trigger')
+            .setURL('https://youtu.be/TYqEcxfYMt0')
+            .setImage('https://i.redd.it/hz43b4ia84l51.png')
+            .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+        message.channel.send(tigerembed);
         return;
-	}
-	if (message.content.toLowerCase().startsWith(`${PREFIX}aww`)) {
-		randomPuppy('aww').then(url => {
-			message.channel.send(url);
-        });  
-        return;
-	}
+    }
 
     if (message.content.toLowerCase().startsWith(`${PREFIX}bonk`)) 
     {
         var whichbonk = Math.floor(Math.random() * 4);
         switch (whichbonk) {
             case 0:
-                message.channel.send('https://cdn.discordapp.com/attachments/686015484281225241/722533493435007066/posterbonk.png');
+                const bonk1embed = new Discord.MessageEmbed()
+                    .setTitle('Bonk and Scout Wedding Photo')
+                    .setURL('https://youtu.be/kuNixp-wvWM')
+                    .setImage('https://cdn.discordapp.com/attachments/686015484281225241/722533493435007066/posterbonk.png')
+                    .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+                message.channel.send(bonk1embed);
                 break;
             case 1:
-                message.channel.send('https://cdn.discordapp.com/attachments/735495269034098771/747478778254459089/posterbonk2.png');
+                const bonk2embed = new Discord.MessageEmbed()
+                    .setTitle('Bonk and Scout going home after the wedding')
+                    .setURL('https://youtu.be/kuNixp-wvWM')
+                    .setImage('https://cdn.discordapp.com/attachments/735495269034098771/747478778254459089/posterbonk2.png')
+                    .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+                message.channel.send(bonk2embed);
                 break;
             case 2:
-                message.channel.send('https://cdn.discordapp.com/attachments/735495269034098771/747478781454712833/posterbonk3.png');
+                const bonk3embed = new Discord.MessageEmbed()
+                    .setTitle('Scout seeing Bonk for the first time')
+                    .setURL('https://youtu.be/kuNixp-wvWM')
+                    .setImage('https://cdn.discordapp.com/attachments/735495269034098771/747478781454712833/posterbonk3.png')
+                    .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+                message.channel.send(bonk3embed);
                 break;
             case 3:
-                message.channel.send('https://cdn.discordapp.com/attachments/735495269034098771/747478775633018970/posterbonk4.png');
+                const bonk4embed = new Discord.MessageEmbed()
+                    .setTitle('Scout and Bonk laughing at Sniper')
+                    .setURL('https://youtu.be/kuNixp-wvWM')
+                    .setImage('https://cdn.discordapp.com/attachments/735495269034098771/747478775633018970/posterbonk4.png')
+                    .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
+                message.channel.send(bonk4embed);
                 break;
             default:
-                message.channel.send('https://cdn.discordapp.com/attachments/686015484281225241/722533493435007066/posterbonk.png');
+                message.channel.send('How did we get here?');
         }
         return
-    	console.log("Hey can somebody keep track of my heads batted in? BONK");
     }
 
 });
---
+++ modules/ksoftCommands.js
@@ -0,0 +1,134 @@
+// This module uses the KSoft.Si API. You need to get your own app approved to use this module.
+// If your app did not get approved, then bad luck. If you do not have a token, register one!
+// Until then, comment the entire module.
+
+const Discord = require('discord.js');
+const { client, PREFIX } = require('../index'); // Import client from index.js
+const fetch = require('node-fetch');
+const ksoftkey = `Bearer ${process.env.KSOFTSI_TOKEN}`; // This is the token you get if your KSoft app is approved
+
+client.on('message', async (message) => 
+{
+    if (message.author.bot) return;
+    if (!message.guild) return;
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}hentai`)) {
+        if (message.channel.nsfw == true) {
+            fetchredditi('https://api.ksoft.si/images/rand-reddit/hentai', message);
+        }
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}animeme`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/goodanimemes', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}dankmeme`)) {
+        fetchredditi('https://api.ksoft.si/images/random-meme', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}sbubby`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/sbubby', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}comedyheaven`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/comedyheaven', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}pic`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/pic', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}4chan`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/greentext', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}aww`)) {
+        fetchredditi('https://api.ksoft.si/images/rand-reddit/aww', message);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}randomnsfw`) || message.content.toLowerCase().startsWith(`${PREFIX}nsfw`)) {
+        if (message.channel.nsfw == true) {
+            fetchredditi('https://api.ksoft.si/images/random-nsfw', message);
+        }
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}porn`)) {
+        if (message.channel.nsfw == true) {
+            fetchredditi('https://api.ksoft.si/images/rand-reddit/porn', message);
+        }
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}rule34`)) {
+        if (message.channel.nsfw == true) {
+            fetchredditi('https://api.ksoft.si/images/rand-reddit/rule34', message);
+        }
+        return;
+    } 
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}wikihow`)) {
+        var deetails = await fetch('https://api.ksoft.si/images/random-wikihow', {
+            method: 'get',
+            headers: { 'Authorization': ksoftkey },
+        });
+        var deetailsjson = await deetails.json();
+        var imageurl = await deetailsjson.url;
+        var embedtitle = await deetailsjson.title;
+        var embedtitleurl = await deetailsjson.article_url;
+        const wikiembed = new Discord.MessageEmbed()
+            .setTitle(embedtitle)
+            .setURL(embedtitleurl)
+            .setImage(imageurl)
+            .setFooter(`Invoked by ${message.author.username}, thanks to KSoft.Si for providing this amazing API.`, message.author.avatarURL());
+        message.channel.send(wikiembed);
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith(`${PREFIX}lyrics`)) {
+        const args = message.content.slice(PREFIX.length).trim().split(' ');
+        const text = message.content.split(args[1] + " ")[1];
+        const lyrics = await fetch(`https://api.ksoft.si/lyrics/search?q=${text}&limit=1`, {
+           headers: { 'Authorization': ksoftkey },
+        });
+        var lyricsjson = await lyrics.json();
+        const lyricsembed = new Discord.MessageEmbed()
+            .setTitle('Lyrics')
+            .setThumbnail(lyricsjson.data[0].album_art)
+            .addFields(
+                { name: 'Artist Name', value: lyricsjson.data[0].artist, inline: true },
+                { name: 'Song Album', value: lyricsjson.data[0].album, inline: true },
+                { name: 'Album Date', value: lyricsjson.data[0].album_year, inline: true },
+                { name: 'Song Name', value: lyricsjson.data[0].name, inline: true },
+            )
+            .setDescription(lyricsjson.data[0].lyrics)
+            .setFooter(`Invoked by ${message.author.username}, thanks to KSoft.Si for providing this amazing API.`, message.author.avatarURL());
+        message.channel.send(lyricsembed);
+    }
+	
+});
+
+async function fetchredditi(link, message) {
+    var details = await fetch(link, {
+        method: 'get',
+        headers: { 'Authorization': ksoftkey },
+    });
+    var detailsjson = await details.json();
+    var imageurl = await detailsjson.image_url;
+    var embedtitle = await detailsjson.title;
+    const redditembed = new Discord.MessageEmbed()
+        .setTitle(embedtitle)
+        .setURL(detailsjson.source)
+        .setImage(imageurl)
+        .setColor(`RANDOM`)
+        .setFooter(`Invoked by ${message.author.username}, thanks to KSoft.Si for providing this amazing API.`, message.author.avatarURL());
+    message.channel.send(redditembed);
+}
