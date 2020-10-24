e3be4b750db0cb1ea9ab210bbc6154f2f1b56a35
DavidJoacaRo <davidjoacaro99.dd95@gmail.com>
2020-10-24 16:40:31 +0300

    e

--- modules/imageCommands.js
+++ modules/imageCommands.js
@@ -26,7 +26,7 @@ client.on('message', async (message) =>
         return;
     }
 
-    if (message.content.toLowerCase().startsWith(`${PREFIX}requestavatar`) || message.content.toLowerCase().startsWith(`${PREFIX}ra`)) 
+    if (message.content.toLowerCase().startsWith(`${PREFIX}avatar`) || message.content.toLowerCase().startsWith(`${PREFIX}a`)) 
     {
         const user = message.mentions.users.first();
         if (user) {
