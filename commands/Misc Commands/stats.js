998978cde15635abdbe0104e8877d1876ed260dd
davidjoacaro <davidjoacaro99.dd95@gmail.com>
2021-02-05 14:32:39 +0200

    other things

--- commands/Misc Commands/stats.js
+++ commands/Misc Commands/stats.js
@@ -3,16 +3,15 @@ const { prefix, botname, gitlink, creator } = require('../../config.json');
 
 module.exports.run = async (client, message, args) => {
     const hembed = new Discord.MessageEmbed()
-    .setTitle(`${botname} stats`)
+    .setTitle(`${botname} statistics`)
 		.setColor("#F2CA82")
 		.setThumbnail('')
 		.addFields(
 			{ name: 'Total guilds', value: client.guilds.cache.size, inline: true },
 			{ name: 'Total users', value: client.users.cache.size, inline: true },
 			{ name: 'Prefix', value: 'dn!', inline: true },
-			{ name: 'Help page', value: '[Click here](https://davidjoacaro.github.io/doughnut/)' }
+			{ name: 'Main page', value: '[Click here](https://davidjoacaro.github.io/doughnut/)' }
 		)
-        .setFooter(`Invoked by ${message.author.username}`, message.author.avatarURL());
     message.channel.send(hembed);
 };
 
